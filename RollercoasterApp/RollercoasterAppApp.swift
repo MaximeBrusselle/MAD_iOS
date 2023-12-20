bb45291a7be89c688c5cc83f84a58080bdd40a6c
maximebrusselle <maxime.brusselle@student.hogent.be>
2023-12-20 20:20:21 +0100

    Initial Commit

---
+++ RollercoasterApp/RollercoasterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollercoasterAppApp.swift
+//  RollercoasterApp
+//
+//  Created by Maxime Brusselle on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RollercoasterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
